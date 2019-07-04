e9575579260044527d7afa4edd927de551ddc21c
JackDong <724083996@qq.com>
2019-07-04 13:55:48 +0800

    第二次提交，spring boot 可以正常使用

--- src/main/java/javabean/IpInfo.java
+++ src/main/java/javabean/IpInfo.java
@@ -4,5 +4,44 @@ public class IpInfo {
     public String Ip;
     public String Domain;
     public String Email;
-    public String
+    public String Location;
+
+    public IpInfo(String ip, String domain, String email, String location) {
+        Ip = ip;
+        Domain = domain;
+        Email = email;
+        Location = location;
+    }
+
+    public String getIp() {
+        return Ip;
+    }
+
+    public void setIp(String ip) {
+        Ip = ip;
+    }
+
+    public String getDomain() {
+        return Domain;
+    }
+
+    public void setDomain(String domain) {
+        Domain = domain;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
 }
