7a265560cceec006da1dd5efa74a59488bd89b68
JackDong <724083996@qq.com>
2019-07-04 13:23:15 +0800

    第一次提交信息，完成spring boot 部署

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,9 @@
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/javabean/IpInfo.java
@@ -0,0 +1,8 @@
+package javabean;
+
+public class IpInfo {
+    public String Ip;
+    public String Domain;
+    public String Email;
+    public String
+}
